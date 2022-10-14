596ea9506f6c58df6542c8a1f45521841473b21a
Shital-1771 <99315804+Shital-1771@users.noreply.github.com>
2022-10-14 18:08:41 +0530

    Add files via upload

---
+++ Thymeleaf_JPA_EmployeesManagement/src/main/java/com/example/demo/repository/EmployeeRepositoryImpl.java
@@ -0,0 +1,24 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EmployeeRepositoryImpl implements EmployeeRepository {
+	@Autowired
+	EntityManager entityManager;
+	
+	public List<Employee> findAll(){
+		Session currentSesssion = entityManager.unwrap(Session.class);
+		Query<Employee> query=currentSesssion.createQuery("select e from Employee e",Employee.class);
+		List<Employee> empList=query.getResultList();
+		
+		return empList;
+	}
+}
